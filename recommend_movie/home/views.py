8913d8c5d22305e7ee607511eb4f891b4b3ac6c0
sweetrain096 <gpfhddl09@gmail.com>
2019-11-26 11:46:08 +0900

    기능: index 페이지 구성
    관련: #1

--- recommend_movie/home/views.py
+++ recommend_movie/home/views.py
@@ -1,10 +1,18 @@
+import random
+
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
   print(request)
-  return HttpResponse("hello, django!")
+  movie_list = ['겨울왕국2', '블랙머니', '조커']
+
+  movie = random.choice(movie_list)
+  return render(request, 'index.html', {'movie': movie})
+  # return HttpResponse("hello, django!")
+
 
 def movie_list(request):
   movie_list = ['겨울왕국2', '블랙머니', '조커']
+
   return render(request, 'movie_list.html', {'movie_list': movie_list})
