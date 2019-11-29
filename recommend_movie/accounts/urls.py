37a72bb261443ad1b4e3678a7d521ee65a3ce7d7
rain Han <gpfhddl09@gmail.com>
2019-11-29 16:39:52 +0900

    기능: 로그아웃 구현 (#8)
    해결: #6

--- recommend_movie/accounts/urls.py
+++ recommend_movie/accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('signin/', views.signin, name='signin'),
+  path('logout/', views.logout, name='logout'),
 ]
